ef6da2d48dfcf6a05525da9edf6db9662b191410
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-09-16 18:34:24 +0200

    Initial spring boot & reactJS app structure

---
+++ src/main/java/ba/atlantbh/auctionapp/AuctionAppApplication.java
@@ -0,0 +1,13 @@
+package ba.atlantbh.auctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionAppApplication.class, args);
+	}
+
+}
