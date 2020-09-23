871ff2b929ca479ebc9fad3b0b3745955c176d11
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-09-23 14:33:44 +0200

    Host part of url extracted to a variable

--- frontend/src/utilities/ServerCalls.js
+++ frontend/src/utilities/ServerCalls.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const hostUrl = 'http://localhost:8080';
+
 export const registerUser = async (user) => {
-    return await axios.post('http://localhost:8080/auth/register', user);
+    return await axios.post(hostUrl + '/auth/register', user);
 };
 
 export const loginUser = async (user) => {
-    return await axios.post('http://localhost:8080/auth/login', user);
+    return await axios.post(hostUrl + '/auth/login', user);
 };
