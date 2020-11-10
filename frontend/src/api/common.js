246a0638e007a4873c4cacd2129a40990c569d31
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-11-10 12:51:51 +0100

    Changed api for geo info & link to default placeholder image now uses https

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -3,7 +3,6 @@ import { getToken } from 'utilities/localStorage';
 export const hostUrl = process.env.REACT_APP_HOST_URL;
 export const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
 export const cloudName = process.env.REACT_APP_CLOUD_NAME;
-export const ipApiKey = process.env.REACT_APP_IP_API_KEY;
 
 export const defaultHeader = () => {
     return {
--- frontend/src/api/geo.js
+++ frontend/src/api/geo.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { getParams, ipApiKey } from './common';
 
 export const getGeoInfo = async () => {
-    return (await axios.get('http://api.ipstack.com/check', getParams({ access_key: ipApiKey }))).data;
+    return (await axios.get('https://ipapi.co/json',)).data;
 };
