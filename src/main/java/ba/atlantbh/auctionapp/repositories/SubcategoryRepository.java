b7caeb1429cbf7834ca03d8c666d8cd6ac3ac083
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-11-11 19:22:12 +0100

    Featured subcategories algorithm improvement

--- src/main/java/ba/atlantbh/auctionapp/repositories/SubcategoryRepository.java
+++ src/main/java/ba/atlantbh/auctionapp/repositories/SubcategoryRepository.java
@@ -12,12 +12,15 @@ import java.util.UUID;
 
 public interface SubcategoryRepository extends JpaRepository<Subcategory, UUID> {
 
-    @Query(value = "SELECT sc.id, sc.name, c.name categoryName, sc.photo_url url, min(start_price) startPrice " +
+    @Query(value = "SELECT * FROM (SELECT sc.id, sc.name, c.name categoryName, sc.photo_url url, min(start_price) startPrice, " +
+                   "(SELECT COUNT(*) FROM bid b INNER JOIN product p2 on p2.id = b.product_id " +
+                   "WHERE b.person_id = :id AND p2.subcategory_id = sc.id) count " +
                    "FROM subcategory sc INNER JOIN category c on c.id = sc.category_id " +
                    "INNER JOIN product p on sc.id = p.subcategory_id " +
                    "WHERE p.start_date <= now() AND p.end_date > now() " +
-                   "GROUP BY (sc.id, sc.name, c.name, sc.photo_url) ORDER BY RANDOM() LIMIT 4", nativeQuery = true)
-    List<SubcategoryProj> getRandomSubcategories();
+                   "GROUP BY (sc.id, sc.name, c.name, sc.photo_url) ORDER BY count DESC, RANDOM() LIMIT 12) main " +
+                   "ORDER BY RANDOM() LIMIT 4", nativeQuery = true)
+    List<SubcategoryProj> getFeaturedSubcategories(String id);
 
     List<SimpleSubcategoryProj> findAllByCategory(Category category);
 }
