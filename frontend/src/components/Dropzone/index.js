8527746a56eb7a6f8c23ee9108209ba44f16ec75
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-11-21 13:39:34 +0100

    Limit to max product image size + error messages for rejected files

--- frontend/src/components/Dropzone/index.js
+++ frontend/src/components/Dropzone/index.js
@@ -5,6 +5,7 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 import update from "immutability-helper";
+import { Form } from "react-bootstrap";
 
 import './dropzone.css';
 
@@ -26,10 +27,11 @@ const Dropzone = ({ onDrop, accept, images, setImages }) => {
 
     const [disabled, setDisabled] = useState(false);
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    const { getRootProps, getInputProps, isDragActive, fileRejections } = useDropzone({
         onDrop,
         accept,
-        disabled
+        disabled,
+        maxSize: 15728640
     });
 
     const moveImage = (dragIndex, hoverIndex) => {
@@ -45,30 +47,49 @@ const Dropzone = ({ onDrop, accept, images, setImages }) => {
         setDisabled(images.length === 10);
     }, [images])
 
+    const getFileErrorMessage = (fileRejections) => {
+        if (fileRejections.length === 0)
+            return;
+        const error = fileRejections[0].errors[0];
+        switch (error.code) {
+            case "file-invalid-type":
+                return "*Invalid image format";
+            case "file-too-large":
+                return "*Images can't be larger than 15 MB";
+            default:
+                return "*" + error.message;
+        }
+    }
+
     return (
-        <div className={getClassName("dropzone", isDragActive)} {...getRootProps()}>
-            <input className="dropzone-input" {...getInputProps()} />
-            <div className="dropzone-content">
-                <DndProvider backend={backendForDND}>
-                    <ImageGrid images={images} setImages={setImages} moveImage={moveImage} />
-                </DndProvider>
-                <div style={{ margin: 'auto 10px' }}>
-                    {isDragActive ? (
-                        "Release to drop the image here"
-                    ) : (
-                            <>
-                                <span style={{ color: 'var(--primary)' }}>
-                                    Upload photos
+        <>
+            <div className={getClassName("dropzone", isDragActive)} {...getRootProps()}>
+                <input className="dropzone-input" {...getInputProps()} />
+                <div className="dropzone-content">
+                    <DndProvider backend={backendForDND}>
+                        <ImageGrid images={images} setImages={setImages} moveImage={moveImage} />
+                    </DndProvider>
+                    <div style={{ margin: 'auto 10px' }}>
+                        {isDragActive ? (
+                            "Release to drop the image here"
+                        ) : (
+                                <>
+                                    <span style={{ color: 'var(--primary)' }}>
+                                        Upload photos
                                 </span>
-                                {' '} or just drag and drop
+                                    {' '} or just drag and drop
                                 <div>
-                                    {images.length > 1 ? "(Tip: Drag the images to change their position)" : null}
-                                </div>
-                            </>
-                        )}
+                                        {images.length > 1 ? "(Tip: Drag the images to change their position)" : null}
+                                    </div>
+                                </>
+                            )}
+                    </div>
                 </div>
             </div>
-        </div>
+            <Form.Control.Feedback style={{ position: 'absolute', width: 'unset' }} className={fileRejections.length > 0 ? "d-block" : null} type="invalid">
+                {getFileErrorMessage(fileRejections)}
+            </Form.Control.Feedback>
+        </>
     );
 };
 
