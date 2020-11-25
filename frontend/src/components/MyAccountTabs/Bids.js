65c7e4fa2853b7e60423d3722c99c2e7bd296403
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-11-25 19:55:35 +0100

    Bids UI done

--- frontend/src/components/MyAccountTabs/Bids.js
+++ frontend/src/components/MyAccountTabs/Bids.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import ProductTable from 'components/ProductTable';
+import { getUserBidProducts } from 'api/product';
 
 import './myAccountTabs.css';
 
 const Bids = () => {
-    
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setProducts(await getUserBidProducts());
+            } catch (e) { }
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [])
+
     return (
-        <div>
-            Bids
-        </div>
+        <>
+            <ProductTable type="bids" products={products} />
+            {loading || products.length === 0 ?
+                <div className="no-table-items font-18">
+                    {loading ? <Spinner className="table-spinner" animation="border" /> : "No bids found"}
+                </div> : null}
+        </>
     );
 }
 
