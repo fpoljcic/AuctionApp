df850c6b9f9534d197f0b46cd0d3362037c01e05
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-11-25 15:52:15 +0100

    User products api endpoint done

---
+++ src/main/java/ba/atlantbh/auctionapp/projections/UserProductProj.java
@@ -0,0 +1,18 @@
+package ba.atlantbh.auctionapp.projections;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface UserProductProj {
+    UUID getId();
+    String getName();
+    String getUrl();
+    BigDecimal getStartPrice();
+    String getCategoryName();
+    String getSubcategoryName();
+    LocalDateTime getStartDate();
+    LocalDateTime getEndDate();
+    Integer getBidCount();
+    BigDecimal getMaxBid();
+}
