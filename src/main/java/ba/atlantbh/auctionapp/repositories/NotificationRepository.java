5625788684b01bb10b739dfe335c862f70db417b
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-12-15 01:00:09 +0100

    Delete notifications with products from deactivated account

--- src/main/java/ba/atlantbh/auctionapp/repositories/NotificationRepository.java
+++ src/main/java/ba/atlantbh/auctionapp/repositories/NotificationRepository.java
@@ -4,10 +4,19 @@ import ba.atlantbh.auctionapp.models.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
     Page<Notification> findAllByPersonIdOrderByDateDesc(UUID uuid, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM notification n USING product p WHERE n.product_id = p.id AND p.person_id = :id",
+            nativeQuery = true)
+    void deleteAllByProductFromUser(String id);
 }
